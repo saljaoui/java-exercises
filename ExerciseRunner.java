8561c54c3efa5fa8cd30f359c87d76c53bc177a9
saljaoui <soufian2022ff@gmail.com>
2025-04-04 14:27:49 +0000

    add HelloWorld exercise solution

--- ExerciseRunner.java
+++ ExerciseRunner.java
@@ -1,5 +1,11 @@
 public class ExerciseRunner {
     public static void main(String []arg) {
-        System.out.println(HelloWorld.helloWorld());
+        System.out.println("Add");
+        System.out.println(FloatOperations.addTwoFloats(1.0f, 2.5f));
+        System.out.println(FloatOperations.addTwoFloats(15.18f, 68.28347f));
+
+        System.out.println("Divide");
+        System.out.println(FloatOperations.divideTwoFloats(7.0f, 2.0f));
+        System.out.println(FloatOperations.divideTwoFloats(5.6f, 6.9f));
     }
 }
