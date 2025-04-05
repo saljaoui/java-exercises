c5d91e6b8c3ff9f72a4af4c31ed0497b6ba28e8e
saljaoui <soufian2022ff@gmail.com>
2025-04-05 11:21:55 +0000

    add ExerciseRunner solution

--- ExerciseRunner.java
+++ ExerciseRunner.java
@@ -1,6 +1,6 @@
 public class ExerciseRunner {
     public static void main(String []arg) {
-        System.out.println(StringLength.getStringLength("Hello World !"));
-        System.out.println(StringLength.getStringLength(""));
+        System.out.println(StringConcat.concat("Hello ", "étudiant !"));
+        System.out.println(StringConcat.concat("", "Hello World !"));
     }
 }
